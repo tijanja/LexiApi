d44d293c0382213fd49bd6fd2baa3c662244c9aa
tijanja007 <adetunji.akinde@techadvance.ng>
2018-06-24 13:57:20 +0100

    recreate the connection file

--- model/Connection.php
+++ model/Connection.php
@@ -3,7 +3,7 @@
 {
     var $db;
     function __construct() {
-        $this->db = new mysqli("localhost","root","Project123");
+        $this->db = new mysqli("localhost","root","Project123","lexi");
         
         if($this->db->connect_errno > 0){
             die('Unable to connect to database [' . $this->db->connect_error . ']');
