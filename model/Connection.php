9cb0b661eeb583f1f7ba30dc8a050cfbe29a74cb
Adetunji Akinde <adetunjiakinde@gmail.com>
2019-05-01 20:53:46 +0100

    changed database connection

--- model/Connection.php
+++ model/Connection.php
@@ -3,7 +3,7 @@
 {
     var $db;
     function __construct() {
-        $this->db = new mysqli("localhost","root","Project123","lexi");
+        $this->db = new mysqli("localhost","tijanja","Mus628cg","lex_nigeria");
         
         if($this->db->connect_errno > 0){
             die('Unable to connect to database [' . $this->db->connect_error . ']');
