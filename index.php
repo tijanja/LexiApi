9bfef1fb5197dc8b62b482168edb360369e18082
tijanja007 <adetunji.akinde@techadvance.ng>
2019-08-18 16:20:59 +0100

    commit test

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-ini_set("allow_url_fopen", true);
+ini_set("allow_url_fopen", "on");
 if (isset($_SERVER['HTTP_ORIGIN'])) {
         header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
         header('Access-Control-Allow-Credentials: true');
