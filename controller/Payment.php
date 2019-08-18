9a84a7cc0401899b54aaf13ca86b738b64ee7b7c
tijanja007 <adetunji.akinde@techadvance.ng>
2019-08-18 17:03:26 +0100

    commit test

--- controller/Payment.php
+++ controller/Payment.php
@@ -2,7 +2,7 @@
 
     if(file_get_contents("http://206.189.115.158/LexiApi/?controller=Merchant&action=partList&statuteId=327"))
     {
-        echo json_decode(file_get_contents("http://206.189.115.158/LexiApi/?controller=Merchant&action=partList&statuteId=327",true),true);
+        print_r(json_decode(file_get_contents("http://206.189.115.158/LexiApi/?controller=Merchant&action=partList&statuteId=327",true),true));
     }
     else
     {
