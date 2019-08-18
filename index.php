e56272e5703c9b4bd3c5a4bd60302088835c46ef
tijanja007 <adetunji.akinde@techadvance.ng>
2019-08-18 17:35:51 +0100

    commit test

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
 
-echo $_SERVER['HTTP_ORIGIN'];
+if(isset($_SERVER['HTTP_ORIGIN']))
+{
+    echo 'it is true';
+}
+else{
+    echo "not true";
+}
 
 if (isset($_SERVER['HTTP_ORIGIN'])) {
         header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
