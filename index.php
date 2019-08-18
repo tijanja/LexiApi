5cb2921eeb2a86e74a17efbf21a26e0a36c2df95
tijanja007 <adetunji.akinde@techadvance.ng>
2019-08-18 17:40:51 +0100

    commit test

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+        echo file_get_contents("https://google.com",true);
+
         header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
         header('Access-Control-Allow-Credentials: true');
         header('Access-Control-Max-Age: 86400'); 
