a9ffc35a1965dac7ea52641cd29d148272b25cee
tijanja007 <adetunji.akinde@techadvance.ng>
2019-08-18 16:57:38 +0100

    commit test

--- controller/Payment.php
+++ controller/Payment.php
@@ -1,3 +1,10 @@
 <?php
 
-echo phpinfo();
+    if(file_get_contents("https://google.com"))
+    {
+        echo file_get_contents("https://google.com",true);
+    }
+    else
+    {
+        throw new Exception("Error cant read url contents");
+    }
