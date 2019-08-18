f7290e54180ebe5fb357a1fecdc69635e1b90a13
tijanja007 <adetunji.akinde@techadvance.ng>
2019-08-18 17:38:52 +0100

    commit test

--- index.php
+++ index.php
@@ -2,19 +2,21 @@
 
         header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
         header('Access-Control-Allow-Credentials: true');
-        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+        header('Access-Control-Max-Age: 86400'); 
+        header("Access-Control-Allow-Methods: GET, POST");
+        header("Access-Control-Allow-Headers:{$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
    
     // Access-Control headers are received during OPTIONS requests
-    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    // if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 
-        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    //     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+    //         header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 
-        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-            header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+    //     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+    //     header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");            
 
-        exit(0); 
-    }
+    //     exit(0); 
+    // }
  
 try
 {
