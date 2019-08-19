add5f09d6c979de9a466376eb2cbd344263c0232
tijanja007 <adetunji.akinde@techadvance.ng>
2019-08-19 12:34:00 +0100

    commit test

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ $obj = json_encode(json_decode($obj1,true),true);
 
 var_dump($obj);
 
-$controller = ucfirst(strtolower(trim($obj['controller'])));
+$controller = ucfirst(strtolower(trim($obj->controller)));
 var_dump($controller);
 $action = strtolower(trim($obj['action']))."Action";
 if(file_exists("controller/{$controller}.php"))
