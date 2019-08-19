50495c57abc7bf1813c5b7620a8a81860a4cb595
tijanja007 <adetunji.akinde@techadvance.ng>
2019-08-19 14:48:01 +0100

    commit test

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 
- //if (isset($_SERVER['HTTP_ORIGIN'])) {
+ if (isset($_SERVER['HTTP_ORIGIN'])) {
         header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
         header('Access-Control-Allow-Credentials: true');
         header('Access-Control-Max-Age: 86400');    // cache for 1 day
         header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
         header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-    //}
+    }
 
     // Access-Control headers are received during OPTIONS requests
     if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
