2679400303c5b7f1970a6ecc932f4396c9a3cbf7
tijanja007 <adetunji.akinde@techadvance.ng>
2019-08-19 14:24:23 +0100

    commit test

--- model/Connection.php
+++ model/Connection.php
@@ -3,7 +3,7 @@
 {
     var $db;
     function __construct() {
-        $this->db = new mysqli("localhost","tijanja","Mus628cg","lex_nigeria");
+        $this->db = new mysqli("","","","");
         
         if($this->db->connect_errno > 0){
             die('Unable to connect to database [' . $this->db->connect_error . ']');
