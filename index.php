ba610279a22087ea39765c2df079ec4183999295
tijanja007 <adetunji.akinde@techadvance.ng>
2019-08-19 15:01:57 +0100

    commit test

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
         header('Access-Control-Allow-Credentials: true');
         header('Access-Control-Max-Age: 86400');    // cache for 1 day
-        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+        header("Access-Control-Allow-Methods: POST");
         header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
     }
 
